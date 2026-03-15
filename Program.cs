40fb62074d29678238f804b381bdc7605928f4f9
rayen <rapriadi706@gmail.com>
2026-03-15 19:18:32 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TP_MODUL04_103022400071;
+
+KodePos kode = new KodePos();
+
+Console.WriteLine("Batununggal: " + kode.getKodePos("Batununggal"));
+Console.WriteLine("Kujangsari: " + kode.getKodePos("Kujangsari"));
+Console.WriteLine("Mengger: " + kode.getKodePos("Mengger"));
+Console.WriteLine("Wates: " + kode.getKodePos("Wates"));
+Console.WriteLine("Cijaura: " + kode.getKodePos("Cijaura"));
+Console.WriteLine("Jatisari: " + kode.getKodePos("Jatisari"));
+Console.WriteLine("Margasari: " + kode.getKodePos("Margasari"));
+Console.WriteLine("Sekejati: " + kode.getKodePos("Sekejati"));
+Console.WriteLine("Kebonwaru: " + kode.getKodePos("Kebonwaru"));
+Console.WriteLine("Maleer: " + kode.getKodePos("Maleer"));
